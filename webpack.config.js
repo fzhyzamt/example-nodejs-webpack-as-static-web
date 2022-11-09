8229fc4153abf5891b6004434dca26cc8031819e
fzhyzamt <fzhyzamt@163.com>
2022-11-09 18:11:21 +0800

    添加多个页面

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
 
     // https://webpack.js.org/concepts/entry-points/#multi-page-application
     entry: {
-        first: './src/first.js'
+        first: './src/first.js',
+        second: './src/second.js',
+        third: './src/third.js'
     },
 
     // https://webpack.js.org/configuration/dev-server/
@@ -17,8 +19,20 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/first.html',
             inject: true,
-            chunks: ['first'],
+            chunks: ['first'], // 这里指定依赖的entry
             filename: 'first.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/second.html',
+            inject: true,
+            chunks: ['second'],
+            filename: 'second.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/third.html',
+            inject: true,
+            chunks: ['third'],
+            filename: 'third.html'
         })
     ]
 };
